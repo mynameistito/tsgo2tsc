71f674351eb2101734af5a3b6f19b9cf047e5996
My Name is Tito <github@mynameistito.com>
2026-07-11 12:04:21 +1200

    chore: restore Ultracite config

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -4,15 +4,4 @@ import core from "ultracite/oxlint/core";
 export default defineConfig({
   extends: [core],
   ignorePatterns: core.ignorePatterns,
-  rules: {
-    // The codebase uses hoisted function declarations and intentionally
-    // performs ordered filesystem operations in several scanners.
-    "eslint/func-style": "off",
-    "eslint/no-await-in-loop": "off",
-    "eslint/no-plusplus": "off",
-    "eslint/no-use-before-define": "off",
-    "eslint/prefer-named-capture-group": "off",
-    "eslint/require-unicode-regexp": "off",
-    "unicorn/import-style": "off",
-  },
 });
